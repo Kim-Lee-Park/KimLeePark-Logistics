8f2a7b886f4e3050702c15f0c16b15f63a418b08
kmr5326 <kmr5326@naver.com>
2025-11-04 17:01:02 +0900

    feat: 허브 서비스 유레카 등록

--- hub/src/main/java/com/klp/hub/HubApplication.java
+++ hub/src/main/java/com/klp/hub/HubApplication.java
@@ -2,8 +2,10 @@ package com.klp.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HubApplication {
 
 	public static void main(String[] args) {
