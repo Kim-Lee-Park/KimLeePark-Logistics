3031d0e6918887548396edc16c53d26ee55c796b
leemk09 <dlaudrb09@naver.com>
2025-11-05 17:30:56 +0900

    test: 단일 상품 조회

---
+++ hub/src/main/java/com/klp/hub/inventory/presentation/controller/InventoryController.java
@@ -0,0 +1,11 @@
+package com.klp.hub.inventory.presentation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/inventories")
+public class InventoryController {
+
+    // 상품 ID 를 통한 인벤토리 조회
+}
