f27f302ed7fe3ad6a637a437eb9974d7692ccb14
leeMK09 <68103540+leeMK09@users.noreply.github.com>
2025-11-05 12:33:19 +0900

    feat: 상품, 업체, 재고 도메인  (#19)
    * feat: 재고 도메인
    
    * feat: 상품 도메인
    
    * feat: Product 와 Inventory 의 연관관계 처리
    
    * improve: Inventory 재고 차감시 예외처리
    
    * feat: Product 와 Company 연관관계 처리
    
    * feat: Company 도메인

---
+++ hub/src/main/java/com/klp/hub/inventory/domain/Inventory.java
@@ -0,0 +1,59 @@
+package com.klp.hub.inventory.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_inventory")
+@Getter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "invevntory_id", nullable = false)
+    private UUID id;
+
+    @Comment("재고 수량")
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Comment("상품 ID")
+    @Column(name = "product_id", nullable = false)
+    private String productId;
+
+    public Inventory() {
+        this.quantity = 0;
+    }
+
+    public Inventory(Integer quantity, String productId) {
+        validQuantity(quantity);
+        this.quantity = quantity;
+        this.productId = productId;
+    }
+
+    public void increase(Integer quantity) {
+        validQuantity(quantity);
+
+        this.quantity += quantity;
+    }
+
+    public void decrease(Integer quantity) {
+        validQuantity(quantity);
+
+        if (this.quantity - quantity < 0) {
+            throw new IllegalArgumentException("기존 재고 수량을 초과하여 차감하였습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+
+    private void validQuantity(Integer quantity) {
+        if (quantity == null || quantity < 0) {
+            throw new IllegalArgumentException("재고 수량은 필수이면서 음수일 수 없습니다.");
+        }
+    }
+}
