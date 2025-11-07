784c5fd6564b0301896bfe43dbd863f87b9587a2
kimmyeongjin <kimmyeongjin@gmail.com>
2025-11-07 15:16:38 +0900

    feat: 멱등키 엔티티 생성

---
+++ delivery/src/main/java/com/klp/common/IdempotencyStatus.java
@@ -0,0 +1,16 @@
+package com.klp.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum IdempotencyStatus {
+
+  PENDING("요청"),
+  COMPLETED("처리 완료"),
+  FAILED("처리 실패");
+
+  private final String description;
+
+}
