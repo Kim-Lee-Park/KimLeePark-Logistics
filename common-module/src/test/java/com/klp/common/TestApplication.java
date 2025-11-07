7e4ccddbcd26b75c0a88eca2113280f3607ab82b
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-11-07 12:21:36 +0900

    feat: 공통 모듈 작성(BaseEntity, Security 설정, 공통 예외처리) (#30)
    * Common Module Initial Commit
    
    * config: 공통 모듈 구성에 필요한 의존성 추가 및 라이브러리 배포 설정
    
    * feat: 공통 예외 처리 기능 작성
    
    * feat: JPA 관련 기능 작성(BaseEntity, AuditorAware)
    
    * feat: 헤더 데이터 기반 SecurityContextHolder에 인증객체 설정 진행
    
    * config: 사용하는 곳에서 Auto-Configuration 가능하도록 데이터 설정
    
    * test: 인가 필터, @PreAuthorize 관련 테스트 코드 작성
    
    * chore: 누락된 파일 추가

---
+++ common-module/src/test/java/com/klp/common/TestApplication.java
@@ -0,0 +1,9 @@
+package com.klp.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+    }
+}
