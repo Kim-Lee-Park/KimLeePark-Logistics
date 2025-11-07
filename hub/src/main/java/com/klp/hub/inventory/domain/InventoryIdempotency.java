f37e0575d42ca310471bbbecb091c3a117443115
leemk09 <dlaudrb09@naver.com>
2025-11-07 11:28:28 +0900

    feat: 멱등키가 빈값에 대한 예외처리

--- hub/src/main/java/com/klp/hub/inventory/domain/InventoryIdempotency.java
+++ hub/src/main/java/com/klp/hub/inventory/domain/InventoryIdempotency.java
@@ -1,24 +1,30 @@
 package com.klp.hub.inventory.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
-import java.util.UUID;
-
 @Entity
 @Table(
-        name = "p_inventory_idempotency",
-        schema = "hub_schema",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "uk_inventory_idempotency_key", columnNames = "idempotency_key"
-                )
-        }
+    name = "p_inventory_idempotency",
+    schema = "hub_schema",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_inventory_idempotency_key", columnNames = "idempotency_key"
+        )
+    }
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InventoryIdempotency {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "invevntory_idempotency_id", nullable = false)
@@ -29,7 +35,7 @@ public class InventoryIdempotency {
     private String idempotencyKey;
 
     public InventoryIdempotency(String idempotencyKey) {
-        if (idempotencyKey == null) {
+        if (idempotencyKey == null || idempotencyKey.isBlank()) {
             throw new IllegalArgumentException("멱등키는 필수값 입니다.");
         }
         this.idempotencyKey = idempotencyKey;
--- hub/src/test/java/com/klp/hub/inventory/domain/InventoryIdempotencyTest.java
+++ hub/src/test/java/com/klp/hub/inventory/domain/InventoryIdempotencyTest.java
@@ -1,9 +1,9 @@
 package com.klp.hub.inventory.domain;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryIdempotencyTest {
 
@@ -11,5 +11,11 @@ class InventoryIdempotencyTest {
     @DisplayName("멱등키가 존재하지 않는다면 예외가 발생한다")
     void nullIdempotencyKey() {
         assertThrows(IllegalArgumentException.class, () -> new InventoryIdempotency(null));
+    }
+
+    @Test
+    @DisplayName("멱등키가 빈 값이라면 예외가 발생한다")
+    void emptyIdempotencyKey() {
+        assertThrows(IllegalArgumentException.class, () -> new InventoryIdempotency(" "));
     }
 }
