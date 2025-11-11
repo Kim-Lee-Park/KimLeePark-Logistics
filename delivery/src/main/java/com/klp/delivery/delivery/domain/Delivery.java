e49cc9c03ac308df7a433822e9447e79ed5c1056
kimmyeongjin <kimmyeongjin@gmail.com>
2025-11-11 22:04:40 +0900

    refactor: Delivery 엔티티 필드 수정 - 주문아이템 ID, 발송업체 ID 추가 - 업체 배송담당자 ID 타입을 Long으로 변경

--- delivery/src/main/java/com/klp/delivery/delivery/domain/Delivery.java
+++ delivery/src/main/java/com/klp/delivery/delivery/domain/Delivery.java
@@ -25,96 +25,106 @@ import org.springframework.util.StringUtils;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Delivery extends BaseEntity {
 
-  @Id
-  @Column(name = "delivery_id", nullable = false)
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID deliveryId;
+    @Id
+    @Column(name = "delivery_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID deliveryId;
 
-  @Comment("허브 경로 계획 ID")
-  @Column(name = "route_plan_id")
-  private UUID routePlanId;
+    @Comment("허브 경로 계획 ID")
+    @Column(name = "route_plan_id")
+    private UUID routePlanId;
 
-  @Comment("업체 배송담당자 ID")
-  @Column(name = "vendor_drvier_id", nullable = false)
-  private UUID vendorDrvierId;
+    @Comment("업체 배송담당자 ID")
+    @Column(name = "vendor_drvier_id", nullable = false)
+    private Long vendorDrvierId;
 
-  @Comment("주문 ID")
-  @Column(name = "order_id", nullable = false)
-  private UUID orderId;
+    @Comment("주문 ID")
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
 
-  @Comment("출발 허브 ID")
-  @Column(name = "departure_id", nullable = false)
-  private UUID departureId;
+    @Comment("주문 아이템 ID")
+    @Column(name = "order_item_id", nullable = false)
+    private UUID orderItemId;
 
-  @Comment("도착 허브 ID")
-  @Column(name = "arrival_id", nullable = false)
-  private UUID arrivalId;
+    @Comment("출발 허브 ID")
+    @Column(name = "departure_id", nullable = false)
+    private UUID departureId;
 
-  @Comment("수령인 ID")
-  @Column(name = "receiver_id", nullable = false)
-  private UUID receiverId;
+    @Comment("도착 허브 ID")
+    @Column(name = "arrival_id", nullable = false)
+    private UUID arrivalId;
 
-  @Comment("수령인 이름")
-  @Column(name = "receiver_name", nullable = false)
-  private String receiverName;
+    @Comment("발송업체 ID")
+    @Column(name = "sender_id", nullable = false)
+    private UUID senderId;
 
-  @Comment("배송지 주소")
-  @Column(name = "address", nullable = false)
-  private String address;
+    @Comment("수령업체 ID")
+    @Column(name = "receiver_id", nullable = false)
+    private UUID receiverId;
 
-  @Comment("수령인 슬랙 ID")
-  @Column(name = "receiver_slack_id", nullable = false)
-  private String receiverSlackId;
+    @Comment("수령업체명")
+    @Column(name = "receiver_name", nullable = false)
+    private String receiverName;
 
-  @Comment("배송경로 ID")
-  @Column(name = "routes_id")
-  private UUID routesId;
+    @Comment("배송지 주소")
+    @Column(name = "address", nullable = false)
+    private String address;
 
-  @Comment("배송상태")
-  @Enumerated(EnumType.STRING)
-  private DeliveryStatus status;
+    @Comment("수령업체 슬랙 ID")
+    @Column(name = "receiver_slack_id", nullable = false)
+    private String receiverSlackId;
 
-  public Delivery(UUID vendorDrvierId, UUID orderId, UUID departureId,
-      UUID arrivalId, UUID receiverId, String receiverName, String address,
-      String receiverSlackId, DeliveryStatus status) {
-    this.vendorDrvierId = vendorDrvierId;
-    this.orderId = orderId;
-    this.departureId = departureId;
-    this.arrivalId = arrivalId;
-    this.receiverId = receiverId;
-    this.receiverName = receiverName;
-    this.address = address;
-    this.receiverSlackId = receiverSlackId;
-    this.status = status;
-  }
+    @Comment("배송경로 ID")
+    @Column(name = "routes_id")
+    private UUID routesId;
 
-  public static Delivery create(UUID vendorDriverId, UUID orderId,
-      UUID departureId, UUID arrivalId, UUID receiverId, String receiverName, String address,
-      String receiverSlackId) {
-    validateDeliveryData(vendorDriverId, receiverName, address, receiverSlackId);
-    return new Delivery(vendorDriverId, orderId, departureId, arrivalId,
-        receiverId, receiverName, address, receiverSlackId,
-        DeliveryStatus.CREATED);
-  }
+    @Comment("배송상태")
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
 
-  private static void validateDeliveryData(UUID vendorDriverId, String receiverName, String address,
-      String receiverSlackId) {
-    if (vendorDriverId == null) {
-      throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "배송 담당자는 필수입니다.");
+    public Delivery(Long vendorDrvierId, UUID orderId, UUID orderItemId, UUID departureId, UUID arrivalId,
+        UUID senderId, UUID receiverId, String receiverName, String address, String receiverSlackId,
+        DeliveryStatus status) {
+        this.vendorDrvierId = vendorDrvierId;
+        this.orderId = orderId;
+        this.orderItemId = orderItemId;
+        this.departureId = departureId;
+        this.arrivalId = arrivalId;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.receiverName = receiverName;
+        this.address = address;
+        this.receiverSlackId = receiverSlackId;
+        this.status = status;
     }
-    if (!StringUtils.hasText(address)) {
-      throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "배송지 주소는 필수입니다.");
-    }
-    if (!StringUtils.hasText(receiverName)) {
-      throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "수령인 이름은 필수입니다.");
-    }
-    if (!StringUtils.hasText(receiverSlackId)) {
-      throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "수령인 슬랙 ID는 필수입니다.");
-    }
-  }
 
-  public void updateStatus(DeliveryStatus status) {
-    this.status = status;
-  }
+    public static Delivery create(Long vendorDriverId, UUID orderId, UUID orderItemId, UUID departureId,
+        UUID arrivalId, UUID senderId, UUID receiverId, String receiverName, String address,
+        String receiverSlackId) {
+        validateDeliveryData(vendorDriverId, receiverName, address, receiverSlackId);
+        return new Delivery(vendorDriverId, orderId, orderItemId, departureId, arrivalId, senderId, receiverId,
+            receiverName, address, receiverSlackId, DeliveryStatus.CREATED);
+    }
+
+    private static void validateDeliveryData(Long vendorDriverId, String receiverName,
+        String address, String receiverSlackId) {
+        if (vendorDriverId == null) {
+            throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "배송 담당자는 필수입니다.");
+        }
+        if (!StringUtils.hasText(address)) {
+            throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "배송지 주소는 필수입니다.");
+        }
+        if (!StringUtils.hasText(receiverName)) {
+            throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA, "수령인 이름은 필수입니다.");
+        }
+        if (!StringUtils.hasText(receiverSlackId)) {
+            throw new BusinessException(DeliveryErrorCode.INVALID_DELIVERY_DATA,
+                "수령인 슬랙 ID는 필수입니다.");
+        }
+    }
+
+    public void updateStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 
 }
