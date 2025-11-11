d6bae4544e1f093a7a3c55a341d4df27ed12ba76
박주찬 <p990805@naver.com>
2025-11-11 12:29:35 +0900

    feat: order 컨틀롤러 단일조회 (Green)

--- order/src/main/java/com/klp/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/klp/order/presentation/controller/OrderController.java
@@ -5,10 +5,14 @@ import com.klp.order.application.service.OrderService;
 import com.klp.order.domain.entity.order.Order;
 import com.klp.order.presentation.dto.order.request.CreateOrderRequest;
 import com.klp.order.presentation.dto.order.response.CreateOrderResponse;
+import com.klp.order.presentation.dto.order.response.GetOneOrderResponse;
 import jakarta.validation.Valid;
 import java.net.URI;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +36,15 @@ public class OrderController {
         URI location = URI.create("/v1/orders/" + response.orderId());
 
         return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<GetOneOrderResponse> getOrder(
+        @PathVariable UUID orderId
+    ) {
+        Order order = orderService.findById(orderId);
+        GetOneOrderResponse response = GetOneOrderResponse.from(order);
+
+        return ResponseEntity.ok(response);
     }
 }
---
+++ order/src/main/java/com/klp/order/presentation/dto/order/response/GetOneOrderResponse.java
@@ -0,0 +1,45 @@
+package com.klp.order.presentation.dto.order.response;
+
+import com.klp.order.domain.entity.order.Order;
+import com.klp.order.domain.entity.order.OrderStatus;
+import com.klp.order.presentation.dto.ordercancellation.response.OrderCancellationResponse;
+import com.klp.order.presentation.dto.orderitem.response.OrderItemResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public record GetOneOrderResponse(
+    UUID orderId,
+    Long supplierId,
+    Long customerId,
+    String comment,
+    OrderStatus orderStatus,
+    List<OrderItemResponse> orderItems,
+    OrderCancellationResponse cancellation,
+    LocalDateTime createdAt,
+    Long createdBy,
+    LocalDateTime updatedAt,
+    Long updatedBy
+) {
+
+    public static GetOneOrderResponse from(Order order) {
+        List<OrderItemResponse> orderItemResponses = order.getOrderItems().stream()
+            .map(OrderItemResponse::from)
+            .collect(Collectors.toList());
+
+        return new GetOneOrderResponse(
+            order.getOrderId(),
+            order.getSupplierId(),
+            order.getCustomerId(),
+            order.getComment(),
+            order.getOrderStatus(),
+            orderItemResponses,
+            OrderCancellationResponse.from(order.getCancellation()),
+            order.getCreatedAt(),
+            order.getCreatedBy(),
+            order.getUpdatedAt(),
+            order.getUpdatedBy()
+        );
+    }
+}
