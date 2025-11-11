3cb896baaa5127dc1d605328d486067440e57f19
kimmyeongjin <kimmyeongjin@gmail.com>
2025-11-11 22:21:01 +0900

    style: 불필요한 공백 제거

--- delivery/src/main/java/com/klp/delivery/delivery/application/command/IdempotencyCommand.java
+++ delivery/src/main/java/com/klp/delivery/delivery/application/command/IdempotencyCommand.java
@@ -4,13 +4,9 @@ import com.klp.delivery.common.IdempotencyStatus;
 import java.util.UUID;
 
 public record IdempotencyCommand(
-
     String idempotencyKey,
-
     UUID orderId,
-
     IdempotencyStatus status
-
 ) {
 
 }
