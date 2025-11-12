fea45b74256dc881277a494512b3276dc9bc7b24
leemk09 <dlaudrb09@naver.com>
2025-11-12 19:09:16 +0900

    feat: 트랜잭셔널 이벤트 리스너의 재고 차감 시도 검증

--- order/src/main/java/com/klp/logistics/product/applicaiton/service/ProductEventListener.java
+++ order/src/main/java/com/klp/logistics/product/applicaiton/service/ProductEventListener.java
@@ -4,12 +4,14 @@ import com.klp.logistics.order.domain.event.OrderCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Service
 public class ProductEventListener {
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void deduct(OrderCreatedEvent event) {
         log.info("재고 차감 order ID : {}", event.orderId());
     }
--- order/src/test/java/com/klp/logistics/product/applicaiton/service/ProductEventListenerTest.java
+++ order/src/test/java/com/klp/logistics/product/applicaiton/service/ProductEventListenerTest.java
@@ -1,7 +1,9 @@
 package com.klp.logistics.product.applicaiton.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -36,10 +38,40 @@ class ProductEventListenerTest {
     );
     private String comments = "comments";
 
-    @Transactional
     @Test
     @DisplayName("주문 생성 이벤트를 구독하여 deduct() 를 수행할 수 있다")
     void subscribeOrderCreatedEvent() {
+        OrderCreateCommand command = new OrderCreateCommand(
+            supplierId,
+            customerId,
+            List.of(product),
+            comments
+        );
+
+        orderService.createOrder(command);
+
+        verify(productEventListener, times(1)).deduct(any(OrderCreatedEvent.class));
+    }
+
+    @Test
+    @DisplayName("주문 생성에 실패한 경우 재고 차감이 시도되지 않는다")
+    void throwOrderCreatedEvent() {
+        OrderCreateCommand invalidCommand = new OrderCreateCommand(
+            supplierId,
+            customerId,
+            List.of(),
+            comments
+        );
+
+        assertThatThrownBy(
+            () -> orderService.createOrder(invalidCommand)
+        ).isInstanceOf(RuntimeException.class);
+        verify(productEventListener, never()).deduct(any(OrderCreatedEvent.class));
+    }
+
+    @Test
+    @DisplayName("주문 생성에 성공한 경우 재고차감이 시도한다")
+    void tryPaySuccess() {
         OrderCreateCommand command = new OrderCreateCommand(
             supplierId,
             customerId,
