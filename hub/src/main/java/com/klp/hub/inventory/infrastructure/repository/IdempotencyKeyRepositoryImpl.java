b46c67c1ae31f5f7383c9332ae8ad7f625dc8453
leemk09 <dlaudrb09@naver.com>
2025-11-17 11:37:52 +0900

    feat: 재고 멱등키 Repository

--- hub/src/main/java/com/klp/hub/inventory/infrastructure/repository/IdempotencyKeyRepositoryImpl.java
+++ hub/src/main/java/com/klp/hub/inventory/infrastructure/repository/IdempotencyKeyRepositoryImpl.java
@@ -1,11 +1,24 @@
 package com.klp.hub.inventory.infrastructure.repository;
 
+import com.klp.hub.inventory.domain.InventoryIdempotency;
 import com.klp.hub.inventory.domain.repository.IdempotencyKeyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
 public class IdempotencyKeyRepositoryImpl implements IdempotencyKeyRepository {
+
+    private final InventoryIdempotencyJpaRepository idempotencyJpaRepository;
 
     @Override
     public boolean tryAcquireIdempotencyKey(String idempotencyKey) {
-        return false;
+        try {
+            idempotencyJpaRepository.saveAndFlush(new InventoryIdempotency(idempotencyKey));
+            return true;
+        } catch (DataIntegrityViolationException exception) {
+            return false;
+        }
     }
 }
