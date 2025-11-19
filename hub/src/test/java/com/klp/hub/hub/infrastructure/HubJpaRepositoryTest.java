b09c9990b576a35c0548262e7043abf271b205aa
leeMK09 <68103540+leeMK09@users.noreply.github.com>
2025-11-19 10:01:55 +0900

    refactor: Hub 모듈 테스트 코드 수정  (#67)
    * fix: 기존 테스트 코드 깨지는 부분 수정
    
    * feat: 재고 멱등키 Repository
    
    * fix: 멱등키에 대한 트랜잭션 분리 작업 롤백
    
    * fix: 멱등키에 대한 추상화 Repository 모델 제거
    
    * fix: 컴파일 에러 부분 수정
    
    * fix: 삭제된 테스트 코드 추가

--- hub/src/test/java/com/klp/hub/hub/infrastructure/HubJpaRepositoryTest.java
+++ hub/src/test/java/com/klp/hub/hub/infrastructure/HubJpaRepositoryTest.java
@@ -2,33 +2,28 @@ package com.klp.hub.hub.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.klp.hub.TestJpaConfig;
 import com.klp.hub.hub.domain.model.Hub;
-import com.klp.hub.hub.infrastructure.HubJpaRepositoryTest.JpaAuditingTestConfig;
 import com.klp.hub.hub.infrastructure.repository.HubJpaRepository;
 import jakarta.persistence.EntityManager;
 import java.util.Optional;
-import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
-@Import(JpaAuditingTestConfig.class)
+@ActiveProfiles("test")
+@Import({
+    TestJpaConfig.class
+})
+@TestPropertySource(properties = {
+    "spring.sql.init.mode=never"
+})
 public class HubJpaRepositoryTest {
-    @TestConfiguration
-    @EnableJpaAuditing
-    static class JpaAuditingTestConfig {
-        @Bean
-        AuditorAware<Long> auditorAware() {
-            return () -> Optional.of(0L);
-        }
-    }
 
     @Autowired
     private HubJpaRepository hubJpaRepository;
@@ -37,8 +32,8 @@ public class HubJpaRepositoryTest {
 
     @Test
     @DisplayName("저장 성공")
-    void save(){
-        Hub hub=Hub.create("test",11.,11.,"testAddress");
+    void save() {
+        Hub hub = Hub.create("test", 11., 11., "testAddress");
 
         hubJpaRepository.save(hub);
 
@@ -48,25 +43,25 @@ public class HubJpaRepositoryTest {
 
     @Test
     @DisplayName("허브ID로 조회 성공")
-    void findByName(){
+    void findByName() {
         //given
-        Hub hub=Hub.create("test",11.,11.,"testAddress");
+        Hub hub = Hub.create("test", 11., 11., "testAddress");
         hubJpaRepository.saveAndFlush(hub);
 
         entityManager.clear();
-        Optional<Hub> result=hubJpaRepository.findById(hub.getHubId());
+        Optional<Hub> result = hubJpaRepository.findById(hub.getHubId());
 
         assertThat(result).isPresent();
     }
 
     @Test
     @DisplayName("허브 이름 존재 여부")
-    void existsByName(){
+    void existsByName() {
         //given
-        Hub hub=Hub.create("test",11.,11.,"testAddress");
+        Hub hub = Hub.create("test", 11., 11., "testAddress");
         hubJpaRepository.save(hub);
 
-        Boolean result=hubJpaRepository.existsByName("test");
+        Boolean result = hubJpaRepository.existsByName("test");
 
         assertThat(result).isTrue();
     }
