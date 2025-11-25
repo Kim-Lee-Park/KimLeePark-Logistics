39bf89df60f3f37d43d6ce50af707659e8610fab
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-11-25 14:08:56 +0900

    feat: 인증 서비스와의 연동을 위한 유저 서비스 엔드포인트 추가 (#79)
    * feat : 유저 서비스 Auditing 적용
    
    * feat : 인증 도메인 연동 API 작성 및 회원가입 승인/거절 작성
    
    * feat : 엔드포인트 변경에 따른 게이트웨이 서비스 public path 값 변경
    
    * remove: 불필요한 테스트 제거
    
    * refactor: 유저를 찾지 못했을 때의 예외를 Infra -> Application 계층으로 변경

--- user/src/main/java/com/klp/user/infrastructure/repository/UserJpaRepository.java
+++ user/src/main/java/com/klp/user/infrastructure/repository/UserJpaRepository.java
@@ -1,6 +1,7 @@
 package com.klp.user.infrastructure.repository;
 
 import com.klp.user.domain.entity.User;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ public interface UserJpaRepository extends JpaRepository<User, Long> {
     boolean existsByName(String username);
 
     Page<User> findAllByNameContaining(String name, String slackId, Pageable pageable);
+
+    Optional<User> findByName(String username);
 }
