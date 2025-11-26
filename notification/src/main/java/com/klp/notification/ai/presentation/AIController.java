b206f0de87dc58dcf77d6ce55689561fcda013bf
dnjsals45 <dnjsals45@naver.com>
2025-11-26 21:56:45 +0900

    feat: AI 컨트롤러 엔드포인트명 변경

--- notification/src/main/java/com/klp/notification/ai/presentation/AIController.java
+++ notification/src/main/java/com/klp/notification/ai/presentation/AIController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/ai")
+@RequestMapping("/v1/notification/ai")
 @RequiredArgsConstructor
 public class AIController {
 
