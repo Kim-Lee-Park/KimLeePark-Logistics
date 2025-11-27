b4924d35932919cafce6ce75cab2ef33a738f3a2
kmr5326 <kmr5326@naver.com>
2025-11-27 16:42:06 +0900

    fix: feign client 예외 처리 수정

--- delivery/src/main/java/com/klp/delivery/routeplan/infrastructure/client/HubFeignClient.java
+++ delivery/src/main/java/com/klp/delivery/routeplan/infrastructure/client/HubFeignClient.java
@@ -1,5 +1,6 @@
 package com.klp.delivery.routeplan.infrastructure.client;
 
+import com.klp.delivery.global.config.HubFeignClientConfig;
 import com.klp.delivery.routeplan.infrastructure.dto.HubResponse;
 import com.klp.delivery.routeplan.infrastructure.dto.HubRouteInfoResponse;
 import java.util.UUID;
@@ -10,14 +11,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@FeignClient(name = "hub-service")
+@FeignClient(name = "hub-service", configuration = HubFeignClientConfig.class)
 public interface HubFeignClient {
 
     @GetMapping("/v1/hubs/{hubId}")
     HubResponse getHubById(@PathVariable UUID hubId);
 
     @GetMapping("/v1/hubs/routes/info")
-    HubRouteInfoResponse getHubRouteInfo(@RequestParam UUID departureId, @RequestParam UUID arrivalId);
+    HubRouteInfoResponse getHubRouteInfo(@RequestParam UUID departureId,
+        @RequestParam UUID arrivalId);
 
     @GetMapping("/v1/hubs/routes/info/all")
     HubRouteInfoResponse getHubRouteInfos();
