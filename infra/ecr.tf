d614ff50597a2c37652f10d2427cb782a99c7954
leemk09 <dlaudrb09@naver.com>
2025-12-01 16:27:43 +0900

    setting: ECR 설정

---
+++ infra/ecr.tf
@@ -0,0 +1,27 @@
+locals {
+  ecr_services = [
+    "discovery",
+    "config",
+    "gateway",
+    "order",
+    "auth",
+    "user",
+    "hub",
+    "delivery",
+    "notification",
+  ]
+}
+
+resource "aws_ecr_repository" "service" {
+  for_each = toset(local.ecr_services)
+
+  name = "${local.project}-${each.key}"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${local.project}-${each.key}-repo"
+  }
+}
