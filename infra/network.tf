2ae784f70e3711cfa75fea6d4fcdfabbbb9c8545
leemk09 <dlaudrb09@naver.com>
2025-12-01 14:25:12 +0900

    setting: infra terraform 설정

---
+++ infra/network.tf
@@ -0,0 +1,166 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${local.project}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.project}-igw"
+  }
+}
+
+resource "aws_subnet" "public_az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_az1
+  availability_zone       = local.az1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.project}-public-az1"
+  }
+}
+
+resource "aws_subnet" "public_az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_az2
+  availability_zone       = local.az2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.project}-public-az2"
+  }
+}
+
+# App Subnet
+resource "aws_subnet" "private_app_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_app_subnet_cidr_az1
+  availability_zone = local.az1
+
+  tags = {
+    Name = "${local.project}-private-app-az1"
+  }
+}
+
+resource "aws_subnet" "private_app_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_app_subnet_cidr_az2
+  availability_zone = local.az2
+
+  tags = {
+    Name = "${local.project}-private-app-az2"
+  }
+}
+
+# DB Subnet
+resource "aws_subnet" "private_db_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_db_subnet_cidr_az1
+  availability_zone = local.az1
+
+  tags = {
+    Name = "${local.project}-private-db-az1"
+  }
+}
+
+resource "aws_subnet" "private_db_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_db_subnet_cidr_az2
+  availability_zone = local.az2
+
+  tags = {
+    Name = "${local.project}-private-db-az2"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${local.project}-public-rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_az1" {
+  subnet_id      = aws_subnet.public_az1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_az2" {
+  subnet_id      = aws_subnet.public_az2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat_eip" {
+  tags = {
+    Name = "${local.project}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_az1" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_az1.id
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${local.project}-nat-az1"
+  }
+}
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_az1.id
+  }
+
+  tags = {
+    Name = "${local.project}-private-app-rtb"
+  }
+}
+
+resource "aws_route_table_association" "private_app_az1" {
+  subnet_id      = aws_subnet.private_app_az1.id
+  route_table_id = aws_route_table.private_app.id
+}
+
+resource "aws_route_table_association" "private_app_az2" {
+  subnet_id      = aws_subnet.private_app_az2.id
+  route_table_id = aws_route_table.private_app.id
+}
+
+resource "aws_route_table" "private_db" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_az1.id
+  }
+
+  tags = {
+    Name = "${local.project}-private-db-rtb"
+  }
+}
+
+resource "aws_route_table_association" "private_db_az1" {
+  subnet_id      = aws_subnet.private_db_az1.id
+  route_table_id = aws_route_table.private_db.id
+}
+
+resource "aws_route_table_association" "private_db_az2" {
+  subnet_id      = aws_subnet.private_db_az2.id
+  route_table_id = aws_route_table.private_db.id
+}
---
+++ infra/variables.tf
@@ -0,0 +1,61 @@
+variable "project_name" {
+  type    = string
+  default = "klp-logistics"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_az1" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr_az2" {
+  type    = string
+  default = "10.0.3.0/24"
+}
+
+variable "private_app_subnet_cidr_az1" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_app_subnet_cidr_az2" {
+  type    = string
+  default = "10.0.4.0/24"
+}
+
+variable "private_db_subnet_cidr_az1" {
+  type    = string
+  default = "10.0.10.0/24"
+}
+
+variable "private_db_subnet_cidr_az2" {
+  type    = string
+  default = "10.0.20.0/24"
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "allowed_ssh_cidr" {
+  description = "SSH 허용 IP"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "ec2_key_name" {
+  description = "EC2 Keypair"
+  type        = string
+  default     = "klp-keypair"
+}
