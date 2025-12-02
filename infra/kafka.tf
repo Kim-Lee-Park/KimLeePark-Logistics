722984f4a3f2c37af399fb04a0bd564c45d18a73
leemk09 <dlaudrb09@naver.com>
2025-12-02 10:47:27 +0900

    fix: kafka 브로커를 멀티 AZ 로 변경

--- infra/kafka.tf
+++ infra/kafka.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "kafka_broker" {
   ami           = data.aws_ami.al2023_x86.id
   instance_type = "t3.small"
   key_name      = var.ec2_key_name
-  subnet_id     = aws_subnet.private_app_az1.id
+
+  subnet_id = count.index == 2 ? aws_subnet.private_app_az2.id : aws_subnet.private_app_az1.id
+
   vpc_security_group_ids = [aws_security_group.kafka.id]
 
   root_block_device {
