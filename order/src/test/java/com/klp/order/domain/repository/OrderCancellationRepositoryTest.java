2fddb78cf9cf4894a29c646a86f032f271f64097
leemk09 <dlaudrb09@naver.com>
2025-12-08 10:59:30 +0900

    fix: order repository 테스트시 postgresql 을 사용, github action 에서 에러 발생

--- order/src/test/java/com/klp/order/domain/repository/OrderCancellationRepositoryTest.java
+++ order/src/test/java/com/klp/order/domain/repository/OrderCancellationRepositoryTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@Disabled
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
