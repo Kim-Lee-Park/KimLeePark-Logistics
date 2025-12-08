530f1f520c29911aea000519f35b2b6dfcf90e0b
leemk09 <dlaudrb09@naver.com>
2025-12-08 16:41:42 +0900

    feat: auth 배포 테스트

--- auth-service/src/main/java/com/klp/authservice/auth/presentation/controller/AuthController.java
+++ auth-service/src/main/java/com/klp/authservice/auth/presentation/controller/AuthController.java
@@ -17,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -88,5 +89,10 @@ public class AuthController {
         return ResponseEntity.ok()
             .headers(headers)
             .body(response);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
