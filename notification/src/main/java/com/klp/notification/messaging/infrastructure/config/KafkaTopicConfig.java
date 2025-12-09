de37e6032e559bb9cbfb6366c931e27f24547757
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-12-09 10:25:29 +0900

    feat: 알림 서비스 Kafka 설정 (#176)

---
+++ notification/src/main/java/com/klp/notification/messaging/infrastructure/config/KafkaTopicConfig.java
@@ -0,0 +1,21 @@
+package com.klp.notification.messaging.infrastructure.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    public static final String DELIVERY_CREATED_TOPIC = "delivery.topic";
+    public static final String NOTIFICATION_DLT = "notification.dlt";
+
+    @Bean
+    public NewTopic notificationDltTopic() {
+        return TopicBuilder.name(NOTIFICATION_DLT)
+            .partitions(3)
+            .replicas(1)
+            .build();
+    }
+}
