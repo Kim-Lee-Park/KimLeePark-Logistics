3e1f76026ea15ca4ca0400ebd0dbc0eebbc5276d
dnjsals45 <dnjsals45@naver.com>
2025-12-09 10:23:35 +0900

    chore: 띄어쓰기 문제 해결

--- user/src/main/java/com/klp/user/infrastructure/client/HubClient.java
+++ user/src/main/java/com/klp/user/infrastructure/client/HubClient.java
@@ -1,17 +1,17 @@
 package com.klp.user.infrastructure.client;
 
 import com.klp.user.global.config.HubFeignClientConfig;
+import com.klp.user.infrastructure.client.dto.request.NearestHubRequest;
 import com.klp.user.infrastructure.client.dto.response.GetHubIdResponse;
 import com.klp.user.infrastructure.client.dto.response.HubDetailResponse;
-import java.util.UUID;
-import com.klp.user.infrastructure.client.dto.request.NearestHubRequest;
 import com.klp.user.infrastructure.client.dto.response.NearestHubResponse;
+import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "hub-service", configuration = HubFeignClientConfig.class)
 public interface HubClient {
@@ -21,6 +21,7 @@ public interface HubClient {
 
     @GetMapping("/v1/hubs/{hubId}")
     HubDetailResponse getHubById(@PathVariable("hubId") UUID hubId);
+
     @PostMapping("/v1/internal/hubs/nearest")
     NearestHubResponse getNearestHub(@RequestBody NearestHubRequest request);
 }
