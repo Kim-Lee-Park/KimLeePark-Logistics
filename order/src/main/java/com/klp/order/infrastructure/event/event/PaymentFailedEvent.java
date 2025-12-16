f36f9860b37d0ff1ee0bec842a2615e438cc2dbd
박주찬 <p990805@naver.com>
2025-12-17 00:07:07 +0900

    fix: 결제 실패 이벤트 제대로 받도록 수정

--- order/src/main/java/com/klp/order/infrastructure/event/event/PaymentFailedEvent.java
+++ order/src/main/java/com/klp/order/infrastructure/event/event/PaymentFailedEvent.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record PaymentApprovedFailedEvent(
+public record PaymentFailedEvent(
     UUID paymentId,
     UUID orderId,
     Long userId,
