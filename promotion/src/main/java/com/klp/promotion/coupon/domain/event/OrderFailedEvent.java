eeff5ca18c393af8ee728873d74f8006ece4e7e9
박주찬 <p990805@naver.com>
2025-12-17 19:14:19 +0900

    refactor: 쿠폰쪽 주문 실패했을 때 처리 기능 추가

--- promotion/src/main/java/com/klp/promotion/coupon/domain/event/OrderFailedEvent.java
+++ promotion/src/main/java/com/klp/promotion/coupon/domain/event/OrderFailedEvent.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 public record OrderFailedEvent(
     UUID orderId,
-    UUID userCouponId
+    UUID userCouponId,
+    WhichRollback type
 ) {
+
 }
--- promotion/src/main/java/com/klp/promotion/coupon/infrastructure/kafka/config/KafkaConsumerConfig.java
+++ promotion/src/main/java/com/klp/promotion/coupon/infrastructure/kafka/config/KafkaConsumerConfig.java
@@ -3,6 +3,8 @@ package com.klp.promotion.coupon.infrastructure.kafka.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.klp.promotion.coupon.domain.event.InventoryDeductedEvent;
 import com.klp.promotion.coupon.domain.event.InventoryDeductedFailedEvent;
+import com.klp.promotion.coupon.domain.event.OrderCancelledEvent;
+import com.klp.promotion.coupon.domain.event.OrderCreatedEvent;
 import com.klp.promotion.coupon.domain.event.OrderFailedEvent;
 import com.klp.promotion.coupon.domain.event.PaymentApprovedEvent;
 import com.klp.promotion.coupon.domain.event.PaymentCancelledEvent;
@@ -67,9 +69,12 @@ public class KafkaConsumerConfig {
             "PaymentApprovedEvent:" + PaymentApprovedEvent.class.getName() + "," +
                 "PaymentCancelledEvent:" + PaymentCancelledEvent.class.getName() + "," +
                 "PaymentFailedEvent:" + PaymentFailedEvent.class.getName() + "," +
-                "InventoryDeductedFailedEvent:" + InventoryDeductedFailedEvent.class.getName() + "," +
+                "InventoryDeductedFailedEvent:" + InventoryDeductedFailedEvent.class.getName() + ","
+                +
                 "InventoryDeductedEvent:" + InventoryDeductedEvent.class.getName() + "," +
-            "OrderFailedEvent:" + OrderFailedEvent.class.getName()
+                "OrderCreatedEvent:" + OrderCreatedEvent.class.getName() + "," +
+                "OrderCancelledEvent:" + OrderCancelledEvent.class.getName() + "," +
+                "OrderFailedEvent:" + OrderFailedEvent.class.getName()
         );
 
         return new DefaultKafkaConsumerFactory<>(props,
